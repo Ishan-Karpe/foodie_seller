3437ef944c021a347980489efbfb874c5878029d
Ishan-Karpe <eshaankarpe@gmail.com>
2022-08-21 20:26:31 -0700

    UI Register Screen in development, first pub package added.

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_seller/widgets/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -7,9 +8,23 @@ class RegisterScreen extends StatefulWidget {
   State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen> {
+class _RegisterScreenState extends State<RegisterScreen>
+{
+  TextEditingController anyController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          InkWell(
+            child: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.20,
+              backgroundColor: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
