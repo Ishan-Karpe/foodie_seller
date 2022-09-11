d9aa4d5c3e1817238f52402b2709eb0f78ac4871
Ishan-Karpe <eshaankarpe@gmail.com>
2022-09-10 20:57:38 -0700

    Both Auth Screens base done. All the functionally is coming next.

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_seller/widgets/custom_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +9,63 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Image.asset(
+                  "images/seller.png",
+                  height: 270,
+              ),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.email_sharp,
+                  controller: emailController,
+                  hintText: "Email",
+                  isObsecure: false,
+                ),
+                CustomTextField(
+                  data: Icons.lock,
+                  controller: passwordController,
+                  hintText: "Password",
+                  isObsecure: true,
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.cyan,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            ),
+            onPressed: () => print("CLICKED"),
+            child: const Text("Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    );
   }
 }
