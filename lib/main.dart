204161b15f192e3916ee8cb10c2419946ba6a55d
Ishan-Karpe <eshaankarpe@gmail.com>
2023-01-08 05:17:47 -0800

    GeoLocator Ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie_seller/splashScreen/splash_screen.dart';
 
-Future<void> main() async {
+
+Future<void> main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+
   runApp(const MyApp());
 }
 
